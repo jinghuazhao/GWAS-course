bb50ed7f0de7b954f47abef2912a8df972acbf20
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2017-02-23 16:21:37 +0000

    HESS

--- hess.sh
+++ hess.sh
@@ -5,8 +5,6 @@ rt=/genetics/bin/hess
 rt2=/genetics/bin/hess/tests
 for chr in $(seq 22)
 do
-  gunzip -c $rt/refpanel/1kg_phase3_chr${chr}_eur_5pct_legend.txt.gz | \
-  awk '{print chr,$1,sprintf("%00000000015d",$2),$2,$3,$4}' chr=$chr | sort -k3 > $chr
   for trait in BUA VOS
   do
      sge "/bin/bash hess.subs $chr $trait $rt $rt2"
@@ -14,4 +12,6 @@ do
   #  /bin/bash hess.subs $chr $trait $rt $rt2 &
   done
 done
-for chr in $(seq 22); do rm $chr BUA-$chr VOS-$chr; done
+# for chr in $(seq 22); do rm $chr BUA-$chr VOS-$chr; done
+
+# parallel -j10 -C' ' '/bin/bash hess.subs {1} {2} /genetics/bin/hess /genetics/bin/hess/tests' ::: $(seq 22) ::: BUA VOS
