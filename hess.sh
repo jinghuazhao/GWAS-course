543a9db0e44d0d606f53f0991feaab4b70b70719
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2017-02-24 22:39:10 +0000

    HESS

---
+++ hess.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# 24-2-2017 MRC-Epid JHZ
+
+trait=$1
+HESS=$2
+wd=$3
+
+cd $wd
+
+for chr in $(seq 22); do 
+    gunzip -c $HESS/refpanel/1kg_phase3_chr${chr}_eur_5pct_legend.txt.gz | \
+    sort -k1,1 | \
+    awk -vOFS="\t" '{print NR,$1,0,$2,$3,$4}' > legend${chr}.txt
+done
+
+parallel -j10 {1} {2} {3} {4} ::: $(seq 22) ::: $trait ::: $HESS ::: $wd
+
+# Step 2 - compute local SNP heritability
+  
+python $HESS/hess.py --prefix $trait --k 50 --out $trait.h2g
+
+rm legend*.txt
