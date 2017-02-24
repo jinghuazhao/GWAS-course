f7f47b6a753202f1921bf177193c098e35a47df9
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2017-02-24 21:00:11 +0000

    A test using chromosomal positions

--- test.sh
+++ test.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # 24-2-2017 MRC-Epid JHZ
 
+rt=/genetics/bin/hess
+rt2=/genetics/bin/hess/tests
+
 for trait in BUA VOS
 do
   for chr in $(seq 22) 23 25;do echo rsID pos A0 A1 Z-score N > tests/$trait-$chr.txt;done
@@ -17,16 +20,19 @@ done
 
 parallel -j10 -C' ' '/bin/bash test.subs {1} {2} /genetics/bin/hess /genetics/bin/hess/tests' ::: $(seq 22) ::: BUA VOS
 
+# Step 2 - compute local SNP heritability
+
+python hess.py --prefix $rt2/BUA --k 50 --out $rt2/BUA.h2g
+python hess.py --prefix $rt2/VOS --k 50 --out $rt2/VOS.h2g
+
 # with sge or none
-rt=/genetics/bin/hess
-rt2=/genetics/bin/hess/tests
 for chr in $(seq 22)
 do
   for trait in BUA VOS
   do
-     sge "/bin/bash hess.subs $chr $trait $rt $rt2"
-  #  /bin/bash hess.subs $chr $trait $rt $rt2 &
+     echo sge "/bin/bash hess.subs $chr $trait $rt $rt2"
+     echo /bin/bash hess.subs $chr $trait $rt $rt2 &
   done
 done
-# for chr in $(seq 22); do rm $chr BUA-$chr VOS-$chr; done
 
+for chr in $(seq 22); do rm $chr BUA-$chr VOS-$chr; done
