9800a6a3f8751e64498ef31a29c8d7d8e6d8490c
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2017-02-24 15:38:35 +0000

    a test using chromosomal positions

--- test.sh
+++ test.sh
@@ -15,6 +15,9 @@ do
   }' trait=$trait
 done
 
+parallel -j10 -C' ' '/bin/bash test.subs {1} {2} /genetics/bin/hess /genetics/bin/hess/tests' ::: $(seq 22) ::: BUA VOS
+
+# with sge or none
 rt=/genetics/bin/hess
 rt2=/genetics/bin/hess/tests
 for chr in $(seq 22)
@@ -22,10 +25,8 @@ do
   for trait in BUA VOS
   do
      sge "/bin/bash hess.subs $chr $trait $rt $rt2"
-  #  try the following line if there is no sge
   #  /bin/bash hess.subs $chr $trait $rt $rt2 &
   done
 done
 # for chr in $(seq 22); do rm $chr BUA-$chr VOS-$chr; done
 
-# parallel -j10 -C' ' '/bin/bash test.subs {1} {2} /genetics/bin/hess /genetics/bin/hess/tests' ::: $(seq 22) ::: BUA VOS
