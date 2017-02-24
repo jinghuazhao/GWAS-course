675b52b824852348a6f72304189f639759644437
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2017-02-24 11:34:56 +0000

    script for QUS

---
+++ QUS.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# 24-2-2017 MRC-Epid JHZ
+
+HESS=/genetics/bin/hess
+wd=/genetics/bin/hess/QUS
+
+cd $wd
+for trait in BUA VOS; do
+    gunzip -c ${trait}.ALL.GWAMA.out.gz | \
+    awk -vOFS="\t" '(NR>1){print $1,NR,$2,$3,$9,$16}' | \
+    sort -k1,1 > ${trait}.txt
+done
+for chr in $(seq 22); do 
+    gunzip -c $HESS/refpanel/1kg_phase3_chr${chr}_eur_5pct_legend.txt.gz | \
+    sort -k1,1 | \
+    awk -vOFS="\t" '{print NR,$1,0,$2,$3,$4}' > legend${chr}.txt
+done
+
+parallel -j10 -C' ' '/bin/bash QUS.subs {1} {2} /genetics/bin/hess /genetics/bin/hess/QUS' ::: $(seq 22) ::: BUA VOS
