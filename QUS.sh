5ea94e7b995179eecfce453002d07e415dab3034
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2017-02-25 07:44:58 +0000

    QUS example

--- QUS.sh
+++ QUS.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # 25-2-2017 MRC-Epid JHZ
 
+cd /genetics/bin/hess/QUS
 for trait in BUA VOS; do
     gunzip -c ${trait}.ALL.GWAMA.out.gz | \
     awk -vOFS="\t" '(NR>1){print $1,NR,$2,$3,$9,$16}' | \
