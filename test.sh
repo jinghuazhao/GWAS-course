28a69377374475c532d1c42e8ad50e321732c701
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2017-03-03 15:55:27 +0000

    A test using chromosomal positions

--- test.sh
+++ test.sh
@@ -1,38 +1,39 @@
 #!/bin/bash
-# 24-2-2017 MRC-Epid JHZ
+# 3-3-2017 MRC-Epid JHZ
 
 rt=/genetics/bin/hess
-rt2=/genetics/bin/hess/tests
+wd=/genetics/data/gwas/8-7-15
 
+cd $wd
 for trait in BUA VOS
 do
-  for chr in $(seq 22) 23 25;do echo rsID pos A0 A1 Z-score N > tests/$trait-$chr.txt;done
-  gunzip -c /genetics/data/gwas/8-7-15/OMICS-EPIC_ALL_${trait}_12122015.txt.gz|awk '
+  for chr in $(seq 22); do echo rsID pos A0 A1 Z-score N > QUS/$trait.$chr.txt; done
+  gunzip -c OMICS-EPIC_ALL_${trait}_12122015.txt.gz|awk -vtrait=$trait '
   {
      if (NR>1) {
         split($2,a,":")
         chr=a[1]
         pos=a[2]
-        print $1, pos, $5, $6, $8/$9, $4 >> sprintf("tests/%s-%d.txt",trait,chr)
+        print $1, pos, $5, $6, $8/$9, $4 >> sprintf("QUS/%s.%d.txt",trait,chr)
      }
-  }' trait=$trait
+  }'
 done
 
-parallel -j10 -C' ' '/bin/bash test.subs {1} {2} /genetics/bin/hess /genetics/bin/hess/tests' ::: $(seq 22) ::: BUA VOS
+# Step 0/1 - setup/eigenvalues and projections
 
-# Step 2 - compute local SNP heritability
-
-python hess.py --prefix $rt2/BUA --k 50 --out $rt2/BUA.h2g
-python hess.py --prefix $rt2/VOS --k 50 --out $rt2/VOS.h2g
-
-# with sge or none
-for chr in $(seq 22)
-do
-  for trait in BUA VOS
-  do
-     echo sge "/bin/bash hess.subs $chr $trait $rt $rt2"
-     echo /bin/bash hess.subs $chr $trait $rt $rt2 &
+# sge
+for chr in $(seq 22); do
+  for trait in BUA VOS; do
+     sge "$wd/test.subs $chr $trait $rt $wd"
   done
 done
 
-for chr in $(seq 22); do rm $chr BUA-$chr VOS-$chr; done
+# parallel -j10 $wd/test.subs {1} {2} {3} {4} ::: $(seq 22) ::: BUA VOS ::: $rt ::: $wd
+
+# Step 2 - compute local SNP heritability
+
+python $rt/hess.py --prefix $wd/QUS/BUA --k 50 --out $wd/QUS/BUA.h2g
+python $rt/hess.py --prefix $wd/QUS/VOS --k 50 --out $wd/QUS/VOS.h2g
+
+for chr in $(seq 22); do rm QUS/$chr QUS/${trait}.${chr}.txt; done
+
