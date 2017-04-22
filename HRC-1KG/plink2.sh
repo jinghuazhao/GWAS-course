404ed9f6cba583b42fc08bb18b97b84ed4afb2c1
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2017-04-22 17:40:32 +0100

    GLGC/GIANT analysis

---
+++ HRC-1KG/plink2.sh
@@ -0,0 +1,16 @@
+# 22-4-2017 MRC-Epid JHZ
+
+s=/genetics/data/omics/EPICNorfolk/Axiom_UKB_EPICN_release_04Dec2014
+t=/scratch/tempjhz22/23-1-17
+# prune SNPs
+/genetics/bin/plink2 --bfile $s --maf 0.05 --hwe 0.000001 --indep-pairwise 50 5 0.2 --threads 12 --out $t/prune
+
+# obtain relationship matrix
+/genetics/bin/plink2 --bfile $s --extract $t/prune.prune.in --make-rel square --threads 12 --out $t/rel
+
+gzip $t/rel.rel
+
+# add header
+awk '{if(NR==1) printf "FID\tIID\t"; printf $2 "\t"}' $t/rel.rel.id | sed 's/[\t]$/\n/g'> $t/EPIC-Norfolk.rel
+gunzip -c $t/rel.rel.gz | paste $t/rel.rel.id - | awk -vFS="\t" '{$1=NR;print}' >> $t/EPIC-Norfolk.rel
+
