637da20b32158d97dc68d36c895d3e56a01baf5b
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2017-05-03 12:14:18 +0100

    SpiroMeta HRC conbtribution

--- SpiroMeta/snpstats.sh
+++ SpiroMeta/snpstats.sh
@@ -1,4 +1,4 @@
-# 2-5-2017 MRC-Epid JHZ
+# 3-5-2017 MRC-Epid JHZ
 
 rt=/gen_omics/data/EPIC-Norfolk/HRC/raw/EPIC-Norfolk
 rt2=/scratch/tempjhz22/23-1-17
@@ -9,7 +9,7 @@ for chr in $(seq 22);do sge "$qctool \
  -g $rt.chr${chr}.bgen \
  -s $rt2/EPIC-Norfolk.sample \
  -incl-samples $rt2/EPIC-Norfolk.inc \
- -snp-stats $rt2/HRC/SpiroMeta/EPIC-Norfolk.chr${chr}.snpstats \
+ -snp-stats $rt2/HRC/SpiroMeta/EPIC-Norfolk.chr${chr}.snpstats.gz \
  -assume-chromosome ${chr} \
  -log $rt2/HRC/SpiroMeta/EPIC-Norfolk.chr${chr}.log"
 done
