fd00be361de1e792c3e4148a56f96cb8a3454f2f
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2017-05-08 13:51:59 +0100

    SpiroMeta HRC conbtribution

--- SpiroMeta/snpstats.sh
+++ SpiroMeta/snpstats.sh
@@ -1,4 +1,4 @@
-# 3-5-2017 MRC-Epid JHZ
+# 8-5-2017 MRC-Epid JHZ
 
 rt=/gen_omics/data/EPIC-Norfolk/HRC/raw/EPIC-Norfolk
 rt2=/scratch/tempjhz22/23-1-17
@@ -14,3 +14,17 @@ for chr in $(seq 22);do sge "$qctool \
  -log $rt2/HRC/SpiroMeta/EPIC-Norfolk.chr${chr}.log"
 done
 
+parallel -j10 '/bin/gunzip -c /scratch/tempjhz22/23-1-17/HRC/SpiroMeta/EPIC-Norfolk.chr{}.snpstats.gz|\
+/bin/awk -vchr={} -f /genetics/data/gwas/27-2-17/snpstats.awk > /scratch/tempjhz22/23-1-17/HRC/SpiroMeta/EPIC-Norfolk.chr{}.info' ::: $(seq 22)
+
+cp /scratch/tempjhz22/23-1-17/HRC/SpiroMeta/EPIC-Norfolk.chr1.info /scratch/tempjhz22/23-1-17/HRC/SpiroMeta/EPIC-Norfolk.info
+for chr in $(seq 2 22);do
+awk 'NR>1' /scratch/tempjhz22/23-1-17/HRC/SpiroMeta/EPIC-Norfolk.chr${chr}.info >> /scratch/tempjhz22/23-1-17/HRC/SpiroMeta/EPIC-Norfolk.info
+done
+stata <<END
+insheet using /scratch/tempjhz22/23-1-17/HRC/SpiroMeta/EPIC-Norfolk.info, case clear
+rename chromosome chr
+rename position pos
+rename information Imp_info
+saveold /scratch/tempjhz22/23-1-17/HRC/SpiroMeta/EPIC-Norfolk, replace 
+END
