d99455027ae36bfdf9100232921ed0d04fe1f4a1
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-04-24 17:01:54 +0100

    batch in Bash

--- st.sh
+++ st.sh
@@ -2,15 +2,32 @@
 
 git add .gitignore
 git commit -m ".gitignore"
-git add README.md
-git commit -m "README"
 git add doc
 git commit -m "Documents and auxiliary files"
-for i in $(seq 5)
-do
-    git add $i
-    git commit -m "Day $i"
-done
+git add 1
+git commit -m "Overview"
+git add 2
+git commit -m "Elements of genetic association"
+git add 3
+git commit -m "GWAS"
+git add 4
+git commit -m "Advanced topics"
+git add 5
+git commit -m "Additional topics"
+git add README.md README.docx
+git commit -m "Outlines for a workshop"
+git add DATA.md
+git commit -m "Data"
+git add REFERENCES.md
+git commit -m "References"
+git add SOFTWARE.md
+git commit -m "Software"
+git add CHARGE
+git commit -m "CHARGE gene-lifestyle interaction analysis"
+git add GLGC-GIANT
+git commit -m "GLGC-GIANT rare variant contribution"
+git add SpiroMeta
+git commit -m "SpiroMeta HRC conbtribution"
 git add st.sh
-git commit -m "st.sh"
+git commit -m "batch in Bash"
 git push
